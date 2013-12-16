8d3287bf4e4c5c9f892484376ad2979a14981c41
Koji HIJIKURO <koji0903@gmail.com>
2013-12-16 23:40:59 +0900

    merge database_saku/login and modify for execution

--- NIPPO/Program.cs
+++ NIPPO/Program.cs
@@ -16,7 +16,8 @@ namespace NIPPO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginWindow());
+            //            Application.Run(new LoginWindow());
+            Application.Run(new MainWindow());
         }
     }
 }
