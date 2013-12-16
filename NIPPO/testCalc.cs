503a59ad1d66a5c6015dbb0bddd0e5d614b4a837
Makoto Sakuragi <makoragi@gmail.com>
2013-12-16 14:16:05 +0900

    add test project.

---
+++ NIPPO/testCalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NIPPO
+{
+
+    public class testCalc
+    {
+        public int Add(int p, int p_2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NIPPO_test/testCalc_test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// 追加：参照設定へnunit.frameworkの追加も必要
+using NUnit.Framework;
+// 追加：参照設定へNIPPOクラスの追加も必要
+using NIPPO;
+
+namespace NIPPO_test
+{
+    // 追加：[TestFixture]で、テスト用のクラスであることを宣言
+    [TestFixture]
+    public class testCalc_test
+    {
+        // 追加：ここにイチからテストを書く
+        [Test]
+        public void CUIでのテスト()
+        {
+            testCalc calc = new testCalc();
+            int result = calc.Add(1, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
