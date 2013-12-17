c92b9ce71ce460e7874c8511a331a5136c055ca8
Makoto Sakuragi <makoragi@gmail.com>
2013-12-17 14:10:02 +0900

    add file.

---
+++ NIPPO/MonthlyReportWindow.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace NIPPO
+{
+    partial class MonthlyReportWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MonthlyReportWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 437);
+            this.Name = "MonthlyReportWindow";
+            this.Text = "MonthlyReportWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
