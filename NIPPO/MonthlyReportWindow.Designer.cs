6a36bf67cb50308844916a5c4e55b81a1b3f6225
Makoto Sakuragi <makoragi@gmail.com>
2013-12-26 15:04:33 +0900

    デザインの微調整

--- NIPPO/MonthlyReportWindow.Designer.cs
+++ NIPPO/MonthlyReportWindow.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle45 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle55 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle46 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle47 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle48 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle49 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle50 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle51 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle52 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle53 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle54 = new System.Windows.Forms.DataGridViewCellStyle();
             this.FYMonth_label = new System.Windows.Forms.Label();
             this.nextMonth_button = new System.Windows.Forms.Button();
             this.prevMonth_button = new System.Windows.Forms.Button();
@@ -110,6 +112,14 @@
             // 
             this.list_dataGridView.AllowUserToAddRows = false;
             this.list_dataGridView.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle45.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle45.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle45.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle45.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle45.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle45.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle45.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.list_dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle45;
             this.list_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.list_dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.day_TextBox,
@@ -128,6 +138,14 @@
             this.list_dataGridView.MultiSelect = false;
             this.list_dataGridView.Name = "list_dataGridView";
             this.list_dataGridView.ReadOnly = true;
+            dataGridViewCellStyle55.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle55.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle55.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle55.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle55.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle55.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle55.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.list_dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle55;
             this.list_dataGridView.RowHeadersVisible = false;
             this.list_dataGridView.RowTemplate.Height = 16;
             this.list_dataGridView.RowTemplate.ReadOnly = true;
@@ -142,23 +160,25 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.editable_toolStripStatusLabel,
             this.user_toolStripStatusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 664);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 660);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(1295, 24);
+            this.statusStrip1.Size = new System.Drawing.Size(1295, 28);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // editable_toolStripStatusLabel
             // 
+            this.editable_toolStripStatusLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
             this.editable_toolStripStatusLabel.Name = "editable_toolStripStatusLabel";
-            this.editable_toolStripStatusLabel.Size = new System.Drawing.Size(54, 19);
+            this.editable_toolStripStatusLabel.Size = new System.Drawing.Size(58, 23);
             this.editable_toolStripStatusLabel.Text = "編集可";
             // 
             // user_toolStripStatusLabel
             // 
+            this.user_toolStripStatusLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
             this.user_toolStripStatusLabel.Name = "user_toolStripStatusLabel";
-            this.user_toolStripStatusLabel.Size = new System.Drawing.Size(132, 19);
+            this.user_toolStripStatusLabel.Size = new System.Drawing.Size(136, 23);
             this.user_toolStripStatusLabel.Text = "表示対象のユーザ名";
             // 
             // summary_groupBox
@@ -287,17 +307,17 @@
             // 
             // outputExcel_button
             // 
-            this.outputExcel_button.Location = new System.Drawing.Point(35, 626);
+            this.outputExcel_button.Location = new System.Drawing.Point(35, 630);
             this.outputExcel_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.outputExcel_button.Name = "outputExcel_button";
-            this.outputExcel_button.Size = new System.Drawing.Size(101, 32);
+            this.outputExcel_button.Size = new System.Drawing.Size(101, 28);
             this.outputExcel_button.TabIndex = 7;
             this.outputExcel_button.Text = "Excel出力";
             this.outputExcel_button.UseVisualStyleBackColor = true;
             // 
             // closeWindow_button
             // 
-            this.closeWindow_button.Location = new System.Drawing.Point(1173, 630);
+            this.closeWindow_button.Location = new System.Drawing.Point(1174, 630);
             this.closeWindow_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.closeWindow_button.Name = "closeWindow_button";
             this.closeWindow_button.Size = new System.Drawing.Size(101, 28);
@@ -309,8 +329,8 @@
             // day_TextBox
             // 
             this.day_TextBox.DataPropertyName = "day";
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            this.day_TextBox.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle46.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.day_TextBox.DefaultCellStyle = dataGridViewCellStyle46;
             this.day_TextBox.HeaderText = "日";
             this.day_TextBox.Name = "day_TextBox";
             this.day_TextBox.ReadOnly = true;
@@ -329,8 +349,8 @@
             // week_TextBox
             // 
             this.week_TextBox.DataPropertyName = "week";
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.week_TextBox.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle47.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.week_TextBox.DefaultCellStyle = dataGridViewCellStyle47;
             this.week_TextBox.HeaderText = "曜日";
             this.week_TextBox.Name = "week_TextBox";
             this.week_TextBox.ReadOnly = true;
@@ -340,90 +360,91 @@
             // start_time_TextBox
             // 
             this.start_time_TextBox.DataPropertyName = "start_time";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle3.Format = "t";
-            dataGridViewCellStyle3.NullValue = null;
-            this.start_time_TextBox.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle48.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle48.Format = "t";
+            dataGridViewCellStyle48.NullValue = null;
+            this.start_time_TextBox.DefaultCellStyle = dataGridViewCellStyle48;
             this.start_time_TextBox.HeaderText = "勤務開始時刻";
             this.start_time_TextBox.Name = "start_time_TextBox";
             this.start_time_TextBox.ReadOnly = true;
             this.start_time_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.start_time_TextBox.Width = 75;
+            this.start_time_TextBox.Width = 70;
             // 
             // end_time_TextBox
             // 
             this.end_time_TextBox.DataPropertyName = "end_time";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle4.Format = "t";
-            dataGridViewCellStyle4.NullValue = null;
-            this.end_time_TextBox.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle49.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle49.Format = "t";
+            dataGridViewCellStyle49.NullValue = null;
+            this.end_time_TextBox.DefaultCellStyle = dataGridViewCellStyle49;
             this.end_time_TextBox.HeaderText = "勤務終了時刻";
             this.end_time_TextBox.Name = "end_time_TextBox";
             this.end_time_TextBox.ReadOnly = true;
             this.end_time_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.end_time_TextBox.Width = 75;
+            this.end_time_TextBox.Width = 70;
             // 
             // rest_time_TextBox
             // 
             this.rest_time_TextBox.DataPropertyName = "rest_time";
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle5.Format = "N2";
-            this.rest_time_TextBox.DefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle50.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle50.Format = "N2";
+            this.rest_time_TextBox.DefaultCellStyle = dataGridViewCellStyle50;
             this.rest_time_TextBox.HeaderText = "休憩時間";
             this.rest_time_TextBox.Name = "rest_time_TextBox";
             this.rest_time_TextBox.ReadOnly = true;
-            this.rest_time_TextBox.Width = 75;
+            this.rest_time_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.rest_time_TextBox.Width = 70;
             // 
             // work_times_TextBox
             // 
             this.work_times_TextBox.DataPropertyName = "work_times";
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle6.Format = "N2";
-            dataGridViewCellStyle6.NullValue = null;
-            this.work_times_TextBox.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle51.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle51.Format = "N2";
+            dataGridViewCellStyle51.NullValue = null;
+            this.work_times_TextBox.DefaultCellStyle = dataGridViewCellStyle51;
             this.work_times_TextBox.HeaderText = "勤務時間";
             this.work_times_TextBox.Name = "work_times_TextBox";
             this.work_times_TextBox.ReadOnly = true;
             this.work_times_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.work_times_TextBox.Width = 75;
+            this.work_times_TextBox.Width = 70;
             // 
             // overtime125_TextBox
             // 
             this.overtime125_TextBox.DataPropertyName = "overtime125";
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle7.Format = "N2";
-            dataGridViewCellStyle7.NullValue = null;
-            this.overtime125_TextBox.DefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle52.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle52.Format = "N2";
+            dataGridViewCellStyle52.NullValue = null;
+            this.overtime125_TextBox.DefaultCellStyle = dataGridViewCellStyle52;
             this.overtime125_TextBox.HeaderText = "普通残業時間";
             this.overtime125_TextBox.Name = "overtime125_TextBox";
             this.overtime125_TextBox.ReadOnly = true;
             this.overtime125_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.overtime125_TextBox.Width = 75;
+            this.overtime125_TextBox.Width = 70;
             // 
             // overtime150_TextBox
             // 
             this.overtime150_TextBox.DataPropertyName = "overtime150";
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle8.Format = "N2";
-            this.overtime150_TextBox.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle53.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle53.Format = "N2";
+            this.overtime150_TextBox.DefaultCellStyle = dataGridViewCellStyle53;
             this.overtime150_TextBox.HeaderText = "深夜残業時間";
             this.overtime150_TextBox.Name = "overtime150_TextBox";
             this.overtime150_TextBox.ReadOnly = true;
             this.overtime150_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.overtime150_TextBox.Width = 75;
+            this.overtime150_TextBox.Width = 70;
             // 
             // holiday_work_times_TextBox
             // 
             this.holiday_work_times_TextBox.DataPropertyName = "holiday_work_times";
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle9.Format = "N2";
-            dataGridViewCellStyle9.NullValue = null;
-            this.holiday_work_times_TextBox.DefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle54.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle54.Format = "N2";
+            dataGridViewCellStyle54.NullValue = null;
+            this.holiday_work_times_TextBox.DefaultCellStyle = dataGridViewCellStyle54;
             this.holiday_work_times_TextBox.HeaderText = "休日出勤";
             this.holiday_work_times_TextBox.Name = "holiday_work_times_TextBox";
             this.holiday_work_times_TextBox.ReadOnly = true;
             this.holiday_work_times_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.holiday_work_times_TextBox.Width = 75;
+            this.holiday_work_times_TextBox.Width = 70;
             // 
             // note_TextBox
             // 
@@ -431,6 +452,7 @@
             this.note_TextBox.HeaderText = "備考";
             this.note_TextBox.Name = "note_TextBox";
             this.note_TextBox.ReadOnly = true;
+            this.note_TextBox.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.note_TextBox.Width = 250;
             // 
             // MonthlyReportWindow
